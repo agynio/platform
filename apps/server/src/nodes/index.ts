3844c5add29f5670b25f472cdbc50ec2bb7c5e6e
Casey Quinn <vitalii+casey-quinn@hautech.ai>
2025-10-16 21:51:28 +0000

    refactor(server): update Agent to use lgnodes imports; drop shim nodes; update index exports

--- apps/server/src/nodes/index.ts
+++ apps/server/src/nodes/index.ts
@@ -1,7 +1,2 @@
 export * from './memory.node';
 export * from './memory.connector.node';
-export * from './lgnodes/base.lgnode';
-export * from './lgnodes/callModel.lgnode';
-export * from './lgnodes/tools.lgnode';
-export * from './lgnodes/summarization.lgnode';
-export * from './lgnodes/enforceRestriction.lgnode';
