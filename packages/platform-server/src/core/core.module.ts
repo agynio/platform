1725a34dd8aaf24471bfc97c90934f23285a2e38
Vitalii Valkov <vitalii@hautech.ai>
2025-10-24 01:11:55 +0200

    Move ncps

--- packages/platform-server/src/core/core.module.ts
+++ packages/platform-server/src/core/core.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from './services/config.service';
 import { LoggerService } from './services/logger.service';
 import { MongoService } from './services/mongo.service';
-import { NcpsKeyService } from './services/ncpsKey.service';
 import { LLMFactoryService } from '../llm/llmFactory.service';
 
 @Module({
@@ -10,14 +9,12 @@ import { LLMFactoryService } from '../llm/llmFactory.service';
     { provide: ConfigService, useFactory: () => ConfigService.fromEnv() },
     LoggerService,
     MongoService,
-    NcpsKeyService,
     LLMFactoryService,
   ],
   exports: [
     ConfigService, //
     LoggerService,
     MongoService,
-    NcpsKeyService,
     LLMFactoryService,
   ],
 })
