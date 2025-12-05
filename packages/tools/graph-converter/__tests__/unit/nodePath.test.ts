2485e6bd842b4ff9351b59addab7683344b2f6dc
Vitalii Valkov <vitalii@hautech.ai>
2025-12-05 16:34:27 +0100

    Delete tool

--- packages/tools/graph-converter/__tests__/unit/nodePath.test.ts
+++
@@ -1,8 +0,0 @@
-import { deriveNodeIdFromPath } from '../../src/fs.js';
-
-describe('deriveNodeIdFromPath', () => {
-  it('decodes URL-encoded ids from path', () => {
-    expect(deriveNodeIdFromPath('nodes/simple-node.json')).toBe('simple-node');
-    expect(deriveNodeIdFromPath('nodes/some%2Fcomplex%20id.json')).toBe('some/complex id');
-  });
-});
