c702572398c1908bb12498d18515113cd32b9f3e
Casey Brooks <casey.brooks@agyn.io>
2025-12-07 05:16:11 +0000

    chore(ui): remove tracing env typing

--- packages/platform-ui/src/vite-env.d.ts
+++ packages/platform-ui/src/vite-env.d.ts
@@ -2,7 +2,6 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL?: string;
-  readonly VITE_TRACING_SERVER_URL?: string;
   readonly VITE_UI_MOCK_SIDEBAR?: string;
 }
 interface ImportMeta {
